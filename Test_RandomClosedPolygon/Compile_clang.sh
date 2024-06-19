fecd4a18197cf3ac41459c9d255fea098af1fab4
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2024-06-19 07:33:04 +0200

    Added some more comments.

--- Test_RandomClosedPolygon/Compile_clang.sh
+++ Test_RandomClosedPolygon/Compile_clang.sh
@@ -1,3 +1,3 @@
-#This is how I compile it on my Apple M1.
+# This is how I compile it on my Apple M1.
 
 clang++ -Wall -Wextra -std=c++20 -Ofast -fno-math-errno -pthread -flto=auto -fenable-matrix -march=native -mtune=native -DNDEBUG -I.. main.cpp -oTest_RandomClosedPolygon_clang
--- Test_RandomClosedPolygon/Compile_gcc.sh
+++ Test_RandomClosedPolygon/Compile_gcc.sh
@@ -1,4 +1,5 @@
 # This is how I compile it on my Apple M1.
+# It works with gcc 14 installed via homebrew. (brew install gcc@14.)
 
 g++-14 -Wall -Wextra -Wno-ignored-qualifiers -std=c++20 -m64 -Ofast -flto=auto -fno-math-errno -pthread -mcpu=apple-m1 -mtune=native -DNDEBUG -I.. main.cpp -oTest_RandomClosedPolygon_gcc
 
